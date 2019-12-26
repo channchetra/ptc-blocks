8449b1e0202fe2a1d5e34425f871e2c735091ac5
channchetra <channchetra@gmail.com>
2019-12-26 16:08:21 +0700

    Update block-1.php

---
+++ blocks/block-1.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://www.channchetra.com
+ * @since      1.0.0
+ *
+ * @package    Ptc_Blocks
+ * @subpackage Ptc_Blocks/blocks
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Ptc_Blocks
+ * @subpackage Ptc_Blocks/blocks
+ * @author     Chann Chetra <chetra-chann@mptc.gov.kh>
+ */
+use Carbon_Fields\Block;
+use Carbon_Fields\Field;
